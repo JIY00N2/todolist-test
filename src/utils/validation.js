150e9350f3fda0e3a35d078f3bf35ac959e056a3
JiYoon <angella990825@gmail.com>
2023-06-20 00:04:23 +0900

    validation 수정

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -3,11 +3,7 @@ export default function todoValidation(todos) {
   if (!Array.isArray(todos)) {
     throw new Error('todos는 배열로 입력해주세요!');
   }
-  if (
-    !todos.every(
-      (item) => typeof item === 'object' && item.hasOwnProperty('text')
-    )
-  ) {
-    throw new Error('todo의 요소는 객체이고 text 속성을 갖져야 합니다.');
+  if (!todos.every((item) => typeof item === 'object')) {
+    throw new Error('todo의 요소는 객체입니다.');
   }
 }
